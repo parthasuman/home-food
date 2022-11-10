583caed0a5e9f5bef467d44925b147debd72dada
parthasuman <parthasuman123@gmail.com>
2022-11-10 22:23:08 +0600

    upload local way

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
@@ -7,7 +8,6 @@ const {
   ListCollectionsCursor,
   ObjectId,
 } = require("mongodb");
-require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
